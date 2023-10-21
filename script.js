84642b75ade489f813abb3f3dd3be930b20a4a00
unknown <sanskarchaurey2000@gmail.com>
2023-10-21 23:58:24 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// const Buttons = document.querySelectorAll(".example_query");
+// const ChatInput = document.querySelector(".chat_inpput");
+// const SendButton = document.querySelector(".send_btn");
+
+// for (const button of Buttons) {
+//     button.addEventListener("click", async (event) => {
+//         const targetElement = event.target;
+//         if (targetElement.classList.contains("example_query")) {
+//             event.stopPropagation();
+//             const ButtonText = targetElement.textContent.trim();
+//             constInput.value = "";
+//             constInput.value = ButtonText;
+//         }
+//     });
+// }
+
+// SendButton.addEventListener("click", () => {
+//     ChatInput.value = "";
+//     prompt("Message Send");
+// });
+
